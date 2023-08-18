a10e80c8a6efe637d99fceb4962819d1ca182d8c
joaquings <cpjags@gmail.com>
2023-08-18 14:11:15 -0300

    added login control - check localStorage

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
+    
+    if (localStorage.getItem('userLoggedIn') === 'true'){
+        const user = localStorage.getItem('username')
+    }
+    else{
+        alert('Detectamos que no has iniciado sesión en este dispositivo. \n Da clic en aceptar para iniciar sesión. ')
+        window.location = "login.html"
+    }
+
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
