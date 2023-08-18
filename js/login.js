d42302f3083a17428216ceb2e47e6ed253cc6730
joaquings <cpjags@gmail.com>
2023-08-17 21:24:48 -0300

    implementacion de localStorage para login

--- js/login.js
+++ js/login.js
@@ -8,7 +8,8 @@ aviso.style.display = 'none'
 function toInicio (event){
     event.preventDefault()
     if (email.value != '' && password.value != ''){
-        location.href ='index.html'
+        validarUser()
+        //location.href ='index.html'
     }else{
         aviso.style.display = 'block'
         setTimeout(()=>{aviso.style.display = 'none'}, 5000)
@@ -16,3 +17,28 @@ function toInicio (event){
 }
 
 button.addEventListener("click", toInicio)
+
+function validarUser(){
+    if (email.value == "admin"){
+        if (password.value == "admin"){
+            localStorage.setItem('userLoggedIn', 'true');
+            localStorage.setItem('username', email.value);
+            console.log('Datos guardados')
+            checkearSesion();
+            location.href = 'index.html'
+        }
+        else{
+            console.log('Error de contrasena')
+        }
+    }
+    else{
+        console.log('Error de usuario')
+    }
+}
+
+function checkearSesion(){
+    if (localStorage.getItem('userLoggedIn') === 'true'){
+        const checkUsername = localStorage.getItem('username');
+        console.log(`Usuario ${checkUsername} ha iniciado sesión.`);
+    }
+}
