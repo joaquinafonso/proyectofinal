7aeda81fca2a16c5d58fadd4e8e509436f8f69d1
Tu Nombre <140862540+carosilvariet@users.noreply.github.com>
2023-08-31 22:29:24 -0300

    filtros2

--- js/products.js
+++ js/products.js
@@ -8,20 +8,27 @@ fetch(API_URL)
     .then(response => response.json())
     .then(data => {
         productList = data.products;
-        const savedFilters = JSON.parse(localStorage.getItem('productFilters')) || {};
+        for (let i=0; i<productList.length; i++) {
+            const paragraph = document.createElement("p");
+            paragraph.innerText = productList[i].name;
+            let div = document.createElement('div')
+            div.classList = ('col-md-3')
 
-        applyFilters(savedFilters);
-    })
+            div.innerHTML = `<div class='card' style='width: auto;'><img src='${productList[i].image}' class='card-img-top' alt='...'><div class='card-body'><h5 class='card-title'>${productList[i].name}</h5><p class='card-text'>${productList[i].description}</p><span class='btn btn-primary btn-price'>${productList[i].currency} ${productList[i].cost}</span></div></div>`
+
+            container.appendChild(div)
+        }    
+    })    
     .catch(error => console.error('Error:', error));
 
-function displayProducts(products) {
+    function displayProducts(products) {
     container.innerHTML = '';
 
     for (let i = 0; i < products.length; i++) {
     }
 }
 
-function applyFilters(minPrice, maxPrice, sortBy) {
+{/* function applyFilters(minPrice, maxPrice, sortBy) {
     let filteredProducts = [...productList];
 
     if (minPrice !== undefined && maxPrice !== undefined) {
@@ -44,9 +51,11 @@ function applyFilters(minPrice, maxPrice, sortBy) {
 function saveFilters(minPrice, maxPrice, sortBy) {
     const filters = { minPrice, maxPrice, sortBy };
     localStorage.setItem('productFilters', JSON.stringify(filters));
-}
+}  */}
 
-const filterForm = document.getElementById('filterForm'); 
+
+
+const filterForm = document.getElementById('filterForm');
 
 filterForm.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -57,4 +66,37 @@ filterForm.addEventListener('submit', function(event) {
 
     applyFilters(minPrice, maxPrice, sortBy);
     saveFilters(minPrice, maxPrice, sortBy);
+});
+
+document.getElementById("clearRangeFilter").addEventListener("click", function(){
+    document.getElementById("rangeFilterCountMin").value = "";
+    document.getElementById("rangeFilterCountMax").value = "";
+
+    minCount = undefined;
+    maxCount = undefined;
+
+    showCategoriesList();
+});
+
+document.getElementById("rangeFilterCount").addEventListener("click", function(){
+    //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
+    //de productos por categoría.
+    minCount = document.getElementById("rangeFilterCountMin").value;
+    maxCount = document.getElementById("rangeFilterCountMax").value;
+
+    if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
+        minCount = parseInt(minCount);
+    }
+    else{
+        minCount = undefined;
+    }
+
+    if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
+        maxCount = parseInt(maxCount);
+    }
+    else{
+        maxCount = undefined;
+    }
+
+    showCategoriesList();
 });
